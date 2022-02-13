aba0bd931d9906111bc773698ca6e7bd2be5855e
guerreroandrey5 <guerreroandrey@gmail.com>
2022-02-13 16:31:24 -0600

    Cambios a la clase sonido

--- src/main/java/Negociacion/sonido.java
+++ src/main/java/Negociacion/sonido.java
@@ -8,9 +8,6 @@ package Negociacion;
  *
  * @author guerrero
  */
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import javax.sound.sampled.AudioInputStream;
@@ -18,38 +15,28 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.swing.JButton;
-import javax.swing.JFrame;
  
-public class sonido extends JFrame implements ActionListener{
-    JButton boton1 = new JButton("Pulsame");
-    public sonido(){
-        super("Pulse el boton");
-        setSize(400,80);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
-        add(boton1);
-        boton1.addActionListener(this);
-        setVisible(true);
-    }
-    public static void main(String[] Dar10){
-        new sonido();
-    }
-    public void ReproducirSonido(String nombreSonido){
+public class sonido{
+    public void ReproducirSonidook(){
        try {
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(nombreSonido).getAbsoluteFile());
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("ok.wav").getAbsoluteFile());
         Clip clip = AudioSystem.getClip();
         clip.open(audioInputStream);
         clip.start();
        } catch(UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
          System.out.println("Error al reproducir el sonido.");
        }
-     }
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == boton1){
-            ReproducirSonido("SanCarlos.wav");
-        }
-    }
+}
+    
+    public void ReproducirSonidono(){
+       try {
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("no.wav").getAbsoluteFile());
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+        clip.start();
+       } catch(UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+         System.out.println("Error al reproducir el sonido.");
+       }
+}
+    
 }
